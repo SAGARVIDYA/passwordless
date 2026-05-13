7f47abccc6d287355ed99e33abdf5c291c928c6f
Vidya Sagar.S <vsagar71004@gmail.com>
2026-05-13 20:22:20 +0530

    Fix Netlify routing

--- src/server/server.js
+++ src/server/server.js
@@ -9,10 +9,7 @@ const app = express();
 
 dotenv.config({ path: './src/server/.env' });
 
-app.use(cors({
-  origin: "https://subtle-basbousa-b52c56.netlify.app",
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
